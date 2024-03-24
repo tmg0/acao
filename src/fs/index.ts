0c6c59e18af57fd630ae0589471de5d322727585
tmg0 <zekun.jin@foxmail.com>
2024-03-24 14:32:30 +0800

    feat: fs read file

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -1,12 +1,20 @@
-import type { AcaoJobStep, RunOptions } from '../core/types'
+import fs from 'node:fs/promises'
+import type { AcaoContext, AcaoJobStep, RunOptions } from '../core/types'
+import { isString } from '../core/utils'
 
-export interface ReadFileOptions extends RunOptions {
+export interface ReadFileOptions extends RunOptions {}
 
-}
+export type ReadFilePath = string | ((prev: any, ctx: AcaoContext) => string | Promise<string>)
 
-export function readFile(_path: string, options: ReadFileOptions): AcaoJobStep {
-  return async function () {
-    const stdout = ''
+export function readFile(path: ReadFilePath, options: ReadFileOptions): AcaoJobStep {
+  return async function (prev: string, ctx: AcaoContext) {
+    const _path = isString(path) ? path : await path(prev, ctx)
+    let stdout = ''
+    const ssh = options.ssh && ctx.ssh
+    if (!ssh)
+      stdout = await fs.readFile(_path, { encoding: 'utf-8' })
+    if (ssh)
+      stdout = (await ctx.ssh?.execCommand(`cat ${_path}`, options)).stdout
     if (!options.transform)
       return stdout
     return options.transform(stdout)
