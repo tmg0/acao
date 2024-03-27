36f92438527a2c7b158af63112cff6f9046e7bfc
tmg0 <zekun.jin@foxmail.com>
2024-03-27 16:45:08 +0800

    chore: just ouput

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { defineCommand } from 'citty'
+import { loadConfig } from 'c12'
 import { description, version } from '../package.json'
 import { checkUpdates } from './core/npm'
+import { createAcao } from './core/context'
+import type { Options } from './core/types'
 
 export const main = defineCommand({
   meta: { name: 'acao', version, description },
@@ -22,5 +25,9 @@ export const main = defineCommand({
   subCommands: {
     run: import('./commands/run').then(r => r.default),
     preview: import('./commands/preview').then(r => r.default),
+  },
+
+  async run({ rawArgs }) {
+    console.log(rawArgs)
   },
 })
