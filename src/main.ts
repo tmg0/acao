5cfd598eba97d8b768095e03bb7636e53eb0af03
tmg0 <zekun.jin@foxmail.com>
2024-03-27 16:43:55 +0800

    chore: ignore run in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { defineCommand } from 'citty'
-import { loadConfig } from 'c12'
 import { description, version } from '../package.json'
 import { checkUpdates } from './core/npm'
-import { createAcao } from './core/context'
-import type { Options } from './core/types'
 
 export const main = defineCommand({
   meta: { name: 'acao', version, description },
@@ -25,12 +22,5 @@ export const main = defineCommand({
   subCommands: {
     run: import('./commands/run').then(r => r.default),
     preview: import('./commands/preview').then(r => r.default),
-  },
-
-  async run({ rawArgs }) {
-    if (rawArgs.length)
-      return
-    const { config } = await loadConfig<Options>({ name: 'acao', rcFile: false })
-    await createAcao(config).runJobs()
   },
 })
