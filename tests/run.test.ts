125b6834d91b6e4b9044d3920b4765359677bf3d
tmg0 <zekun.jin@foxmail.com>
2024-03-27 16:08:52 +0800

    chore: add basic run usage

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,9 +1,15 @@
 import { describe, expect, it } from 'vitest'
 import { execaCommand } from 'execa'
+import { run } from '../src'
 
 describe('run', () => {
   it('execa', async () => {
     const { stdout } = await execaCommand('echo 1')
+    expect(Number(stdout)).toMatchInlineSnapshot(`1`)
+  })
+
+  it('basic', async () => {
+    const stdout = await run('echo 1')(undefined, {} as any)
     expect(Number(stdout)).toMatchInlineSnapshot(`1`)
   })
 })
