bba53a6fa362bce449b100ebdda6cb87df9948d3
tmg0 <zekun.jin@foxmail.com>
2024-03-27 16:23:41 +0800

    chore: output logs

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,13 @@ export const runMain = () => _runMain(main)
 
 export function run(cmd: RunCmd, options: Partial<RunOptions> = {}) {
   return defineRunner(async (prev, ctx) => {
+    console.log('setup runner')
     const _cmd = isString(cmd) ? cmd : await cmd(prev, ctx)
     const ssh = options.ssh && ctx.ssh
+    console.log('running cmd')
+    console.log(`has ssh: ${!!ssh}`)
     const { stdout } = ssh ? await ctx.ssh?.execCommand(_cmd, options) : await execaCommand(_cmd, options)
+    console.log('running cmd')
     if (!options.transform)
       return destr(stdout)
     return options.transform(stdout)
