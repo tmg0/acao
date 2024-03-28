8f4f9c627ffd2e2eb3f99a65ec93411e6d84ccaf
tmg0 <zekun.jin@foxmail.com>
2024-03-28 11:40:40 +0800

    chore: mv fs test

--- tests/fs.test.ts
+++ tests/fs.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { readFile } from '../src'
+import { readFile } from '../packages/fs/src'
 
 describe('fs', () => {
   it('read file', async () => {
