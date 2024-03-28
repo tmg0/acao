616701fc47dabd3395e575a6e96818af125c1100
tmg0 <zekun.jin@foxmail.com>
2024-03-28 11:15:24 +0800

    feat: add runtime config

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,13 +1,11 @@
-import { loadConfig } from 'c12'
 import { defineCommand } from 'citty'
-import type { Options } from '../core/types'
 import { createAcao } from '../core/context'
+import { runtimeConfig } from '../main'
 
 export default defineCommand({
   meta: { name: 'preview', description: 'Preview jobs' },
   async run() {
-    const { config } = await loadConfig<Options>({ name: 'acao', rcFile: false })
-    const ctx = createAcao(config)
+    const ctx = createAcao(runtimeConfig.value)
     console.log(JSON.stringify(ctx.jobs, null, 2))
   },
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { defineCommand, runCommand } from 'citty'
+import { loadConfig } from 'c12'
 import { description, version } from '../package.json'
 import { checkUpdates } from './core/npm'
+import type { Options } from './core/types'
 import run from './commands/run'
 import preview from './commands/preview'
+
+export interface Global {
+  value?: Options | null
+}
+
+export const runtimeConfig: Global = {
+  value: undefined,
+}
 
 export const main = defineCommand({
   meta: { name: 'acao', version, description },
@@ -17,6 +27,11 @@ export const main = defineCommand({
   },
 
   async setup({ args }) {
+    if (!runtimeConfig.value) {
+      const { config } = await loadConfig<Options>({ name: 'acao', rcFile: false })
+      runtimeConfig.value = config
+    }
+
     if (!args.noUpdateNotifier)
       await checkUpdates()
   },
